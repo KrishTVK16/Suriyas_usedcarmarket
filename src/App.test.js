926306746ef876b4a092dcb43c30202f01b1177f
Krishna <vamsitvk225@gmail.com>
2025-12-18 14:48:38 +0530

    Delete src directory

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders UsedCar Market app', () => {
-  render(<App />);
-  const appElement = document.querySelector('.App');
-  expect(appElement).toBeInTheDocument();
-});
